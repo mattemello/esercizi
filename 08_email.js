13941385b92af3c7f85f163053ee26762bfdf6d8
matteomassimello <matteomassimello@github.com>
2024-02-23 15:13:23 +0100

    possibile esercizio 8 completato

--- 08_email.js
+++ 08_email.js
@@ -3,12 +3,42 @@
  * un indirizzo email valido secondo i criteri standard. */
 
 function identific(email) {
-    if (email == "@") {
+    let chiocciola = 0, punto_dopo = 0;
+    for (let i = 0; i < email.length; i++) {
+        switch (email.charAt(i)) {
+            case '@':
+                chiocciola = chiocciola + 1;
+            break;
+
+            case '.':
+                if(chiocciola == 1){
+                    punto_dopo = punto_dopo + 1;
+                }
+            break;
+
+            case 'è':
+            case 'é':
+            case 'ò':
+            case 'à':
+            case 'ù':
+            case 'ì':
+                return false;
+            break;
+
         
+            default:
+                break;
+        }
+    }
+
+    if (chiocciola == 1 && punto_dopo >= 1) {
+        return true;
+    }else{
+        return false;
     }
 }
 
-let email = "prova@libero.it";
+let email = "matteo.massimello@gmail.it";
 
 if (identific(email)) {
     console.log("l'indirizzo è accettato.");
