bdce5f135fac844a2bb761055760d2d59dffd654
matteomassimello <matteomassimello@github.com>
2024-02-23 13:43:05 +0100

    pausa esercizio coda e inizio esercizio email

--- 07_coda.js
+++ 07_coda.js
@@ -4,7 +4,26 @@
  * dequeue (rimuove e restituisce l'elemento dalla testa della coda) 
  * e isEmpty (verifica se la coda è vuota). */
 
-coda => {
-    let next;
-    let valore;
+coda = {
+    next: null,
+    valore,
+    enqueue: function(val) {
+        this.valore = val;
+        next = this.next;
+    },
+
+    dequeue: function() {
+        this.next = null;
+
+        return this.valore;
+    },
+
+    isEmpty: function() {
+        if (this.next == null && this.val == undefined){
+            return true;
+        }
+        return false;
+    }
 }
+
+
---
+++ 08_email.js
@@ -0,0 +1,3 @@
+/**Valida un indirizzo email:
+ *  Scrivi uno script che validi se una stringa rappresenta 
+ * un indirizzo email valido secondo i criteri standard. */
