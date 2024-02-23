8d203bdcfcd1424f96d4d6f11d231cbd5d16fca6
matteomassimello <matteomassimello@github.com>
2024-02-23 13:54:05 +0100

    inizio esercizio email

--- 08_email.js
+++ 08_email.js
@@ -1,3 +1,17 @@
 /**Valida un indirizzo email:
  *  Scrivi uno script che validi se una stringa rappresenta 
  * un indirizzo email valido secondo i criteri standard. */
+
+function identific(email) {
+    if (email == "@") {
+        
+    }
+}
+
+let email = "prova@libero.it";
+
+if (identific(email)) {
+    console.log("l'indirizzo è accettato.");
+} else {
+    console.log("l'indirizzo non è valido.");
+}
