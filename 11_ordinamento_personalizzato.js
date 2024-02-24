7c61258575f1b71244e233facbc1e978b6339620
matteomassimello <matteodragonfire2312@gmail.com>
2024-02-24 21:01:38 +0100

    inizio 11

---
+++ 11_ordinamento_personalizzato.js
@@ -0,0 +1,5 @@
+/**Implementa una funzione di ordinamento personalizzata: 
+ * Scrivi una funzione che accetti un array e restituisca un nuovo array ordinato 
+ * secondo un criterio personalizzato, 
+ * ad esempio in base alla lunghezza delle stringhe o al valore numerico. */
+
