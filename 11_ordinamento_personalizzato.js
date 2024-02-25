a7b98a4c3110ccc1d10dc510260062b20d6b0a73
matteomassimello <matteodragonfire2312@gmail.com>
2024-02-25 11:05:42 +0100

    inzio esercizio 11

--- 11_ordinamento_personalizzato.js
+++ 11_ordinamento_personalizzato.js
@@ -3,3 +3,22 @@
  * secondo un criterio personalizzato, 
  * ad esempio in base alla lunghezza delle stringhe o al valore numerico. */
 
+var global = {
+    stringhe: new Array,
+    num: 0,
+    ordinam: "",
+}
+
+function aggiunta () {
+    global.stringhe[global.num] = document.forms.form.stringa.value;
+    global.num += 1; 
+};
+
+function stamp () {
+    document.getElementById("stampa").innerHTML = "la stringa e': " + global.stringhe;
+}
+
+function ordinamento() {
+    global.ordinam = document.forms.ord.tipo.value;
+    console.log("val: " + global.ordinam);
+}
