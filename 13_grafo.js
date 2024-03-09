7eba72a49a4d0f3a6810b4b9cc8751f0b361023d
matteomassimello <matteodragonfire2312@gmail.com>
2024-03-09 22:12:17 +0100

    devo fare il grafo

---
+++ 13_grafo.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author: Massimello Matteo
+ * 
+ * Crea una struttura dati per un grafo: 
+ * Implementa una struttura dati per rappresentare un grafo
+ *  e aggiungi funzioni per aggiungere e rimuovere vertici e archi, 
+ * nonché per eseguire operazioni come la ricerca del percorso più breve.
+ */
+
+class grafo {
+    constructor(){
+        this.value = null;
+        this.destraGraf = null;
+        this.sinistraGraf = null;
+    }
+}
