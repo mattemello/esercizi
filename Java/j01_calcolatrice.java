19cfa6db6988c7bd02dcea9798637dd92b96c455
matteomassimello <matteodragonfire2312@gmail.com>
2024-03-13 22:21:52 +0100

    inizio dell'esercizio 01

---
+++ Java/j01_calcolatrice.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ * 01_calcolatrice
+ * @author: Massimello Matteo
+ * Crea una calcolatrice che possa eseguire operazioni di base come addizione, 
+ * sottrazione, moltiplicazione e divisione. Assicurati di gestire eventuali errori di input.
+ */
+
+package Java;
+
+import java.util.Scanner;
+
+public class j01_calcolatrice {
+
+    public static Scanner myObj = new Scanner(System.in);
+
+    private static String scan(){
+        return myObj.nextLine();
+    }
+
+    private static float controllOperatore(String oper, String n1, String n2){
+        
+        if (n1 != null && n2 != null) {
+            switch (oper) {
+                case "+":
+                    return (Float.valueOf(n1) + Float.valueOf(n2));
+
+                case "-":
+                    return (Float.valueOf(n1) - Float.valueOf(n2));
+
+                case "*":
+                    return (Float.valueOf(n1) * Float.valueOf(n2));
+
+                case "/":
+                    if (n2.equals("0")) {
+                        throw new IllegalArgumentException("Stai dividendo per zero, non puoi!!!");
+                    }else{
+                        return (Float.valueOf(n1) / Float.valueOf(n2));
+                    }
+            
+                default:
+                    throw new IllegalArgumentException("Scritta nell'operatore non valida!!!");
+            } //switch
+        } //if
+        throw new IllegalArgumentException("Non hai scritto uno dei due valori");
+    }
+
+    public static void main(String[] args) {
+        
+        String num1 = null, oper = null, num2 = null;
+
+        while(true) {
+            System.out.println("____________________________");
+            System.out.println("       Calcolatrice");
+            System.out.println("____________________________");
+            System.out.println("1        2        3        +");
+            System.out.println("4        5        6        -");
+            System.out.println("7        8        9        *");
+            System.out.println("         0                 /");
+            System.out.println("q per uscire/ q for exit    ");
+            System.out.println("____________________________");
+            System.out.print("inserire il primo numero ");
+            num1 = scan();
+            System.out.print("inserire il l'operatore ");
+            oper = scan();
+            System.out.print("inserire il secondo numero ");
+            num2 = scan();
+
+            System.out.print("\033[H\033[2J"); 
+
+            if (num1.equals("q") || num2.equals("q") || oper.equals("q"))
+                break;
+
+            System.out.println("il risultato è: " + controllOperatore(oper, num1, num2));
+            
+        }//while
+
+    }//main
+    
+}
