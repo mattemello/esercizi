bf1bbe6b2d897ac018d767b9f24d241a3d4c27af
matteomassimello <matteodragonfire2312@gmail.com>
2024-03-15 21:41:34 +0100

    creazione delle liste

--- Java/liste.java
+++ Java/liste.java
@@ -7,11 +7,28 @@
 package Java;
 
 public class liste{
-    
-	int elem;
-  
-  public static void main(String[] args) {
-    System.out.println("prova");
+
+	private int elem;
+  private liste next; 
+
+  protected liste() {
+    this.elem = elem;
+    this.next = next;
   }
 
+  public int getElem() {
+    return elem;
+  }
+
+  public liste getNext() {
+    return next;
+  }
+
+  public void setElem(int elem) {
+    this.elem = elem;
+  }
+
+  public void setNext(liste next) {
+    this.next = next;
+  }
 }
