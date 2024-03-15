7bc90cf16d20f67974828c04344928f7491e5477
matteomassimello <matteodragonfire2312@gmail.com>
2024-03-15 13:36:54 +0100

    creazione file liste

---
+++ Java/liste.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Gestione delle liste: Crea un programma che gestisca una lista di elementi, consentendo all'utente di aggiungere, rimuovere o cercare elementi nella lista.
+ * @author: Massimello Matteo
+ */
+
+package Java;
+
+public class liste{
+    
+	int elem;
+  
+  public static void main(String[] args) {
+    System.out.println("prova");
+  }
+
+}
