0386db85ab130fc5b0ef156b34bb8a88f663dbea
matteomassimello <matteodragonfire2312@gmail.com>
2024-03-17 15:37:24 +0100

    creazione funzione

--- C/02_cambio_valuta.c
+++ C/02_cambio_valuta.c
@@ -7,19 +7,29 @@
  *
  */
 
+void scambioMon(int num, char vCam, char vTo){
+     
+     
+
+}
+
 int main(int argc, char *argv[]){
 
    int numero_da_camb;
    char valuta_cam, valuta_da;
 
-   while (true) {
-        printf("inserire la cifra di denaro: ");
-        scanf("%d", numero_da_camb);
-        printf("\n inserire il tipo di valuta del numero inserito: ");
-        scanf(" %c", valuta_da);
-   }
+     while (0) {
+          printf("inserire la cifra di denaro: ");
+          scanf("%d", numero_da_camb);
+          printf("\n inserire il tipo di valuta del numero inserito: ");
+          scanf(" %c", valuta_da);
+          printf("\n inserire il tipo di valuta che si vuole ottenere: ");
+          scanf(" %c", valuta_cam);
 
-    return EXIT_SUCCESS;
+
+     }
+
+    return 0;
 }
 
 
