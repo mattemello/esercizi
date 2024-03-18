48da0a67bb93030b7556097592503abfd1705d2a
matteomassimello <matteodragonfire2312@gmail.com>
2024-03-18 09:55:21 +0100

    continuo esercizio 02

--- C/02_cambio_valuta.c
+++ C/02_cambio_valuta.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  *
@@ -7,30 +8,85 @@
  *
  */
 
-void scambioMon(int num, char vCam, char vTo){
+void scambioMon(int num, char vCam[], char vTo[]){
      
-     switch(vTo){
-          case '$':
-               printf("hai preso ul dollaro \n");
-          break;
-     
-          default:
-          break;
+     float val_eu;
+
+     if (vCam == vTo){
+          printf("errore! hai inserito la stessa valuta!");
+          return;
      }
+     
+
+     if (strcmp(vTo, "$")){
+          val_eu = num * 0.92;               
+     }else if (strcmp(vTo, "£")){
+          val_eu = num * 1.17;
+     }
+     else
+     {
+          /* code */
+     }
+     
+
+     /*switch(vTo){
+          case '¥$':
+               val_eu = num * 0.92;               
+          break;
+     
+          case "£":
+               val_eu = num * 1.17;
+          break;
+
+          case '€':
+               val_eu = num;
+          break;
+
+          case '¥':
+               val_eu = num * 0.0062;
+          break;
+
+          default:
+               printf("Error! valuta non riconosciuta");
+               return;          
+     }
+
+     switch (vCam){
+          case '$':
+               printf("Il valore dei soldi in dollari è: %.2lf", (val_eu * 1.09));               
+          break;
+     
+          case '£':
+               printf("Il valore dei soldi in dollari è: %.2ls ", (val_eu * 1.09) );               
+          break;
+
+          case '€':
+               val_eu = num;
+          break;
+
+          case '¥':
+               val_eu = num * 0.0062;
+          break;
+
+          default:
+               printf("Error! valuta non riconosciuta");
+               return;          
+     }*/
+    
 }
 
 int main(int argc, char *argv[]){
 
    int numero_da_camb;
-   char valuta_cam, valuta_da;
+   char valuta_cam[2], valuta_da[2];
 
      while (1) {
           printf("inserire la cifra di denaro: ");
           scanf("%d", &numero_da_camb);
           printf("\n inserire il tipo di valuta del numero inserito: ");
-          scanf(" %c", &valuta_da);
+          scanf(" %s", &valuta_da);
           printf("\n inserire il tipo di valuta che si vuole ottenere: ");
-          scanf(" %c", &valuta_cam);
+          scanf(" %s", &valuta_cam);
           printf("\n");
 
           scambioMon(numero_da_camb, valuta_cam, valuta_da);
